668dfc65210c5abdc5298b9306e627c5eb0f82ae
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-06-01 19:19:15 -0300

    Assets Page Design

--- frontend/app/ativos/page.tsx
+++ frontend/app/ativos/page.tsx
@@ -2,6 +2,9 @@
 
 import { useQuery } from 'react-query';
 import { api } from '@/lib/api';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { MoveLeft } from "lucide-react";
 
 interface Ativo {
     id: number;
@@ -21,15 +24,30 @@ export default function AtivosPage() {
     if (error) return <p>Erro ao carregar ativos.</p>;
 
     return (
-        <div>
-            <h1>Ativos</h1>
-            <ul>
-                {ativos!.map(ativo => (
-                    <li key={ativo.id}>
-                        {ativo.nome} — {ativo.valor}
-                    </li>
-                ))}
-            </ul>
+        <div className="border border-white border-solid rounded-xl w-full max-w-[50%] min-h-[600px] max-h-screen overflow-auto flex flex-col justify-between font-[family-name:var(--font-geist-sans)] px-4 sm:px-10 py-6 sm:py-10">
+            <div className="flex flex-col items-center w-full">
+                <h1 className="text-xl mb-6 text-center">Ativos</h1>
+                <div className="hidden sm:grid grid-cols-[2fr_1fr] w-full border-b border-gray-300 pb-2 mb-4 text-sm font-semibold text-gray-100 gap-4">
+                    <p>Nome</p>
+                    <p>Valor</p>
+                </div>
+
+                <div className="w-full">
+                    {ativos!.map((ativo) => (
+                        <div key={ativo.id} className="grid grid-cols-1 sm:grid-cols-[2fr_1fr] gap-4 w-full border-b border-gray-200 py-2 text-sm text-white">
+                            
+                            <p><span className="sm:hidden font-semibold">Nome: </span>{ativo.nome}</p>
+                            <p>R$ <span className="sm:hidden font-semibold">Valor: </span>{ativo.valor}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="flex justify-center flex-wrap gap-4 mt-8">
+                <Button asChild variant="ghost">
+                    <Link href="/"><MoveLeft className="mr-2" />Voltar</Link>
+                </Button>
+            </div>
         </div>
     );
 }
