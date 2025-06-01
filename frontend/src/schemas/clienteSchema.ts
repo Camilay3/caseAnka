f801f7ed146d75dc2e8b975cbaf83ebcf8ae4a17
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-05-31 21:10:03 -0300

    Configuração do React

---
+++ frontend/src/schemas/clienteSchema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const clienteSchema = z.object({
+  nome: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
+  email: z.string().email("E-mail inválido"),
+  status: z.boolean(),
+});
+
+export type ClientFormData = z.infer<typeof clienteSchema>;
